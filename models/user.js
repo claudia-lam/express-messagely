d8ef58c57034e88774cdb694192777f811748c17
Evan Hesketh <evanhesketh@yahoo.ca>
2023-04-10 15:39:57 -0400

    add authenticate, updateTimestamp, all on user model

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 "use strict";
 const bcrypt = require("bcrypt");
 const { BCRYPT_WORK_FACTOR } = require("../config");
+const {NotFoundError} = require("../expressError");
 
 /** User of the site. */
 
@@ -15,7 +16,7 @@ class User {
     INSERT INTO users (username, password, first_name, last_name, phone)
     VALUES
     ($1, $2, $3, $4, $5)
-    RETURNING username,password, first_name, last_name, phone
+    RETURNING username, password, first_name, last_name, phone
     `,
       [username, hashedPassword, first_name, last_name, phone]
     );
@@ -25,16 +26,49 @@ class User {
 
   /** Authenticate: is username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) {}
+  static async authenticate(username, password) {
+    const results = await db.query(
+      `SELECT password
+        FROM users
+        WHERE username=$1`,
+        [username]
+    );
+
+    const user = results.rows[0];
+    //TODO: possible refactor to throw error?
+    return await bcrypt.compare(password, user?.password) === true;
+  }
 
   /** Update last_login_at for user */
 
-  static async updateLoginTimestamp(username) {}
+  static async updateLoginTimestamp(username) {
+    const timestamp = new Date();
+    const results = await db.query(
+      `INSERT INTO users (last_login_at)
+        VALUE ($1)
+        WHERE username=$2
+        RETURNING username`,
+        [timestamp, username]
+    );
+
+    if (!results.rows[0]) {
+      throw new NotFoundError();
+    }
+  }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name}, ...] */
 
-  static async all() {}
+  static async all() {
+    const results = await db.query(
+    `SELECT username, first_name, last_name
+      FROM users
+      ORDER BY last_name, first_name`
+    );
+
+    const users = results.rows;
+
+    return users;
 
   /** Get: get user by username
    *
