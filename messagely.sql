427df95d4f80e3fe6611bdcf466718f8c4f4ed8e
Claudia Lam <claudiaslam@gmail.com>
2023-04-10 16:23:52 -0700

    add messagesFrom query

--- messagely.sql
+++ messagely.sql
@@ -23,6 +23,17 @@ CREATE TABLE messages (
   sent_at TIMESTAMP WITH TIME ZONE NOT NULL,
   read_at TIMESTAMP WITH TIME ZONE);
 
+INSERT INTO users (username, password, first_name, last_name, phone, join_at)
+VALUES
+  ('claudiaslam', 'secretpassword', 'claudia', 'lam', '647-78990', '2018-09-08 12:20:07-07'),
+  ('evanhesketh', 'secret', 'evan', 'hesketh', '658-68574', '2018-06-18 19:31:59-07'),
+  ('testUser', 'password', 'user', 'userlastname', '58576-585', '2018-09-08 12:20:07-07');
+
+INSERT INTO messages(from_username, to_username, body, sent_at)
+VALUES
+  ('claudiaslam', 'evanhesketh', 'hi', '2023-04-10 12:20:07-07'),
+  ('claudiaslam', 'testUser', 'test message', '2023-04-10 12:20:07-07');
+
 
 \echo 'Delete and recreate messagely_test db?'
 \prompt 'Return for yes or control-C to cancel > ' foo
--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 "use strict";
 const bcrypt = require("bcrypt");
 const { BCRYPT_WORK_FACTOR } = require("../config");
-const {NotFoundError} = require("../expressError");
+const { NotFoundError } = require("../expressError");
 
 /** User of the site. */
 
@@ -13,12 +13,13 @@ class User {
     const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
     const results = await db.query(
       `
-    INSERT INTO users (username, password, first_name, last_name, phone)
+    INSERT INTO users (username, password, first_name, last_name, phone,
+      join_at)
     VALUES
-    ($1, $2, $3, $4, $5)
+    ($1, $2, $3, $4, $5, $6)
     RETURNING username, password, first_name, last_name, phone
     `,
-      [username, hashedPassword, first_name, last_name, phone]
+      [username, hashedPassword, first_name, last_name, phone, new Date()]
     );
     const user = results.rows[0];
     return user;
@@ -31,12 +32,12 @@ class User {
       `SELECT password
         FROM users
         WHERE username=$1`,
-        [username]
+      [username]
     );
 
     const user = results.rows[0];
     //TODO: possible refactor to throw error?
-    return await bcrypt.compare(password, user?.password) === true;
+    return (await bcrypt.compare(password, user?.password)) === true;
   }
 
   /** Update last_login_at for user */
@@ -48,10 +49,12 @@ class User {
         VALUE ($1)
         WHERE username=$2
         RETURNING username`,
-        [timestamp, username]
+      [timestamp, username]
     );
 
-    if (!results.rows[0]) {
+    const username = results.rows[0];
+
+    if (!username) {
       throw new NotFoundError();
     }
   }
@@ -61,15 +64,15 @@ class User {
 
   static async all() {
     const results = await db.query(
-    `SELECT username, first_name, last_name
+      `SELECT username, first_name, last_name
       FROM users
       ORDER BY last_name, first_name`
     );
 
     const users = results.rows;
-
+    if (!users[0]) throw new NotFoundError("No users found!");
     return users;
-
+  }
   /** Get: get user by username
    *
    * returns {username,
@@ -79,7 +82,19 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) {}
+  static async get(username) {
+    const result = db.query(
+      `
+      SELECT username, first_name, last_name, phone, join_at, last_login_at
+      FROM   users
+      WHERE  username = $1
+      `,
+      [username]
+    );
+    const user = result.rows[0];
+    if (!user) throw new NotFoundError(`No user found for ${username}!`);
+    return user;
+  }
 
   /** Return messages from this user.
    *
@@ -89,7 +104,22 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) {}
+  static async messagesFrom(username) {
+    const result = db.query(
+      `
+      SELECT id, to_username, u.username, u.first_name, u.last_name, u.phone,
+              body, sent_at, read_at
+      FROM   messages as m
+      JOIN   users as u
+      ON     from_username = u.username
+      WHERE  u.username = 'claudiaslam';
+      `,
+      [username]
+    );
+
+    const messages = result.rows;
+    const toUsers = messages.map((message) => message.to_username); //[evanhesketh, testUser]
+  }
 
   /** Return messages to this user.
    *
