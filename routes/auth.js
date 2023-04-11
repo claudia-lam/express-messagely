c0473971198e23b0d1b48edb28ed39ac4e8b47ed
Claudia Lam <claudiaslam@gmail.com>
2023-04-11 08:31:28 -0700

    add auth.js routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,36 @@
 
 const Router = require("express").Router;
 const router = new Router();
+const { BadRequestError, UnauthorizedError } = require("../expressError");
+const User = require("../models/user");
+const { SECRET_KEY } = require("../config");
+const jwt = require("jsonwebtoken");
 
 /** POST /login: {username, password} => {token} */
-
+router.post("/login", async function (req, res, next) {
+  if (req.body === undefined) throw new BadRequestError();
+  const { username, password } = req.body;
+  const validUser = await User.authenticate(username, password);
+  if (validUser) {
+    const token = jwt.sign({ username }, SECRET_KEY);
+    return res.json({ token });
+  }
+  throw new UnauthorizedError("Invalid user/password!");
+});
 
 /** POST /register: registers, logs in, and returns token.
  *
  * {username, password, first_name, last_name, phone} => {token}.
  */
+router.post("/register", async function (req, res, next) {
+  if (req.body === undefined) throw new BadRequestError();
+  const user = await User.register(req.body);
+  const validUser = await User.authenticate(username, password);
+  if (validUser) {
+    const token = jwt.sign({ username }, SECRET_KEY);
+    return res.json({ token });
+  }
+  throw new UnauthorizedError("Invalid user/password!");
+});
 
 module.exports = router;
